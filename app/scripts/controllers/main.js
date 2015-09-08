2228b75e6fbeac53874eea19f9241d17758cfe5a
Anna Miroshnik <anna.miroshnik@jamaza.com>
2015-09-08 15:05:19 +0300

    the main functionality was done (service table with details)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,45 +2,5 @@
 
 angular.module('bookingApp')
   .controller('MainCtrl', ['$http', function ($http) {
-    const self = this;
-    const userEmail = "user@email.com";
 
-    self.endPointBase = "https://acme-sandbox.l27.co/api";
-
-    self.endPoints = {
-      auth: self.endPointBase + "/auth",
-      bookings: self.endPointBase + "/bookings",
-      getBookingServices: self.endPointBase + "/bookings_services"
-    }
-
-    $http.defaults.headers.common['X-API-Key'] = 'test_8kBFkhmf8TA7TZyQBh';
-    $http.defaults.headers.common['Content-Type'] = 'application/json';
-
-
-    $http.post(self.endPoints.auth, {
-      "auth": {
-        "email": userEmail,
-        "password": "trustno1"
-      }
-    })
-      .then(function (response) {
-        let single_access_token = response.data.user.single_access_token;
-        $http.defaults.headers.common['X-Authentication'] = userEmail + ":" + single_access_token;
-
-        $http.get(self.endPoints.bookings)
-          .then(function (booking_response) {
-            self.bookings = booking_response;
-            console.log("!!!! " + JSON.stringify(booking_response));
-          });
-
-      }, function (response) {
-        console.error(response);
-      });
-
-
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   }]);
---
+++ app/scripts/directives/booking-table.directive.js
@@ -0,0 +1,88 @@
+'use strict';
+
+angular.module('bookingApp').directive('bookingList', ['$http', directive])
+
+function directive($http) {
+  return {
+    templateUrl: 'scripts/directives/booking-table.directive.html',
+    restrict: 'E',
+    controller: ['$scope', '$log', '$http', controller],
+    controllerAs: 'ctrl',
+
+    link: function postLink(scope, element, attrs) {
+    }
+  };
+
+  function controller($scope, $log, $http) {
+    const self = this;
+
+    const USER_EMAIL = "user@email.com";
+    const USER_PASSWORD = "trustno1";
+    const API_KEY = "test_8kBFkhmf8TA7TZyQBh";
+
+    const endPointBase = "https://acme-sandbox.l27.co/api";
+    const endPoints = {
+      auth: endPointBase + "/auth",
+      bookings: endPointBase + "/bookings",
+      bookingServices: endPointBase + "/bookings/{0}/services",
+    }
+
+    $http.defaults.headers.common['X-API-Key'] = API_KEY;
+    $http.defaults.headers.common['Content-Type'] = 'application/json';
+
+    String.prototype.format = String.prototype.f = function(){
+      var args = arguments;
+      return this.replace(/\{(\d+)\}/g, function(m,n){
+        return args[n] ? args[n] : m;
+      });
+    };
+
+    self.showDetail = function ($event, booking) {
+       $event.preventDefault();
+
+      if (self.active != booking.id) {
+        self.activeBooking = {
+          id :  booking.id,
+          services: []
+        };
+
+        $http.get(endPoints.bookingServices.f(booking.id))
+          .then(function (booking_response) {
+            self.activeBooking.services = Object.assign(self.activeBooking.services, booking_response.data.services);
+            self.activeBooking.services.map(function(service) {
+              service.extraNames = _.pluck(service.extras, 'name').join(', ');
+              service.pricingParameterNames = _.pluck(service.pricing_parameters, 'name').join(', ');
+            })
+            console.log();
+          });
+      }
+      else {
+        self.activeBooking = null;
+      }
+    }
+
+    self.hideDetails = function ($event) {
+      $event.preventDefault();
+      self.activeBooking = null;
+    }
+
+    $http.post(endPoints.auth, {
+      "auth": {
+        "email": USER_EMAIL,
+        "password": USER_PASSWORD
+      }
+    })
+      .then(function (response) {
+        let single_access_token = response.data.user.single_access_token;
+        $http.defaults.headers.common['X-Authentication'] = USER_EMAIL + ":" + single_access_token;
+
+        $http.get(endPoints.bookings)
+          .then(function (booking_response) {
+            self.bookings = booking_response.data.bookings;
+          });
+
+      }, function (response) {
+        element.text('Error: ' + response);
+      });
+  }
+}
