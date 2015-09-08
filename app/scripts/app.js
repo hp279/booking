bfc79230db5a66c2feb3c6a29fbc19353fcd0159
Anna Miroshnik <anna.miroshnik@jamaza.com>
2015-09-08 11:49:06 +0300

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bookingApp
+ * @description
+ * # bookingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bookingApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
